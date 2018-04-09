fbccfa3ea3d6d8dbe0ed731286cc04eb6e8068f8
wang peng <wangpeng199451@gmail.com>
2018-04-09 09:30:53 +0800

    ...

--- chapter-04/todo/src(官方)/todos/actions.js
+++ chapter-04/todo/src(官方)/todos/actions.js
@@ -1,9 +1,5 @@
 import {ADD_TODO, TOGGLE_TODO, REMOVE_TODO} from './actionTypes.js';
-/**
- * actions.js 文件是用来生成action的构造函数
- * 因为组件调用disatch时传入的action除了type之外
- * 也会携带其他组件内部产生的信息，一同分发到reduce做相应状态字段的计算更新
- */
+
 let nextTodoId = 0;
 
 export const addTodo = (text) => ({
