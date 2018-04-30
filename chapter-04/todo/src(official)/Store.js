8666f0f1b45009f574b5b4131ebe629d4c9fcc7c
Wang Peng <947034046@qq.com>
2018-04-30 17:05:45 +0800

    引入react devtools， redux devtools， reducer纯函数检查等

--- chapter-04/todo/src(official)/Store.js
+++ chapter-04/todo/src(official)/Store.js
@@ -23,4 +23,25 @@ const storeEnhancers = compose(
   (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
 );
 
+// 第二个参数是preloadedState，用于可选初始化state，需要和combineReducers组合成的state保持一致
 export default createStore(reducer, {}, storeEnhancers);
+// 也可以不传第二个参数，第三个参数storeEnhancers默认到了第二个位置
+
+/**
+ * createStore源码
+ */
+// export default function createStore(reducer, preloadedState, enhancer) {
+//   if (typeof preloadedState === 'function' && typeof enhancer === 'undefined') {
+//     enhancer = preloadedState
+//     preloadedState = undefined
+//   }
+
+//   if (typeof enhancer !== 'undefined') {
+//     if (typeof enhancer !== 'function') {
+//       throw new Error('Expected the enhancer to be a function.')
+//     }
+
+//     return enhancer(createStore)(reducer, preloadedState)
+//   }
+//   //...省略很多字
+// }
--- chapter-04/todo/src/Store.js
+++ chapter-04/todo/src/Store.js
@@ -1,3 +1,4 @@
+
 /*
 
 const ALL = 'all';
@@ -23,10 +24,18 @@ const UNCOMPLETED = 'uncompleted';
 
 */
 
-import {createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 import { reducer as todoReducer } from './todos/';
 import { reducer as filterReducer } from './filter/';
+
+/**
+ * 使用性能工具，必须在Store中引入Perf
+ */
+import Perf from 'react-addons-perf';
+
+const win = window;
+win.Perf = Perf;
 
 const reducer = combineReducers({
   todos: todoReducer, // 这里的字段名todos、filter和State中的字段名对应  todoReducer每一次执行出来的结果都作为State的todos字段
@@ -35,4 +44,16 @@ const reducer = combineReducers({
 // 合成后的reducer会将接收到的参数之一state对象拆开处理，todos字段的交给todoReducer处理,另同理
 // 子reducer中接受的state是经过分配后的部分state，所以在各自reducer的实现上，专心处理自己的state，忽略其他staet字段即可
 
-export default createStore(reducer); // 没有给状态初始值？ 在子reducer中最后会给state一个default值，即是初值
+const middlewares = [];
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(require('redux-immutable-state-invariant')());
+}
+
+
+const storeEnhancers = compose(
+  applyMiddleware(...middlewares), // 用于检查reducer纯函数
+  (win && win.devToolsExtension) ? win.devToolsExtension() : f => f, // 为Redux Devtools的使用加入钩子
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default createStore(reducer, {}, storeEnhancers); // 没有给状态初始值？ 在子reducer中最后会给state一个default值，即是初值
