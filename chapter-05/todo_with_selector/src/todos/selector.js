ee0819f125fb4177b770066ed552b1f9189a0297
wangpeng <pengwang14@iflytek.com>
2018-05-04 11:04:30 +0800

    增加selector选择器用来缓存state

--- chapter-05/todo_with_selector/src/todos/selector.js
+++ chapter-05/todo_with_selector/src/todos/selector.js
@@ -4,10 +4,10 @@ import {FilterTypes} from '../constants.js';
 const getFilter = (state) => state.filter;
 const getTodos = (state) => state.todos;
 
-export const selectVisibleTodos = createSelector(
-  [getFilter, getTodos],
-  (filter, todos) => {
-    switch (filter) {
+export const selectVisibleTodos = createSelector( // 创造选择器函数是高阶函数，以函数为参数，返回新的函数
+  [getFilter, getTodos], // 步骤一需要的映射计算
+  (filter, todos) => { // 步骤二计算选择器需要返回的最终数据，参数是步骤一的返回结果
+    switch (filter) { // 步骤二内部和之前写法没有不同，区别在于不是每次都被调用，可能根据步骤一的判断直接使用缓存
       case FilterTypes.ALL:
         return todos;
       case FilterTypes.COMPLETED:
